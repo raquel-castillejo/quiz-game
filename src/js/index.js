23378f510cf7a600d62a977f4d1c13d72df798ef
Raquel Castillejo <125091177+raquel-castillejo@users.noreply.github.com>
2023-11-23 19:45:18 +0100

    game coded

--- src/js/index.js
+++ src/js/index.js
@@ -15,8 +15,90 @@ LÓGICA
 - listado con las respuestas correctas e incorrectas
 */
 
+import { QUESTIONS } from './questions';
+
 // DOM
 // ===
 
-// pillar la pregunta y el contenedor de las respuestas
 // las respuestas las puedo comparar y saber solo usando el textcontent
+const questionElement = document.getElementById('question');
+const answersElements = document.getElementById('answers');
+
+// VARIABLES
+// =========
+
+const answers = [];
+let round = 0;
+
+// FUNCIONES
+// =========
+
+// comprobar las respuestas
+const checkAnswers = () => {
+	const questionContainer = document.getElementById('question-container');
+
+	questionElement.remove();
+	answersElements.remove();
+
+	const fragment = document.createDocumentFragment();
+
+	const resultTitle = document.createElement('span');
+	resultTitle.textContent = 'Resultado final';
+	resultTitle.classList.add('results-title');
+	fragment.append(resultTitle);
+
+	answers.forEach((answer, index) => {
+		const questionData = QUESTIONS[index];
+
+		const roundQuestion = document.createElement('span');
+		roundQuestion.textContent = questionData.question;
+		roundQuestion.classList.add('round-question');
+
+		const roundAndUserAnswers = document.createElement('span');
+		roundAndUserAnswers.classList.add('round-answers');
+
+		const userAnswer = document.createElement('span');
+		userAnswer.textContent = answer;
+		answer === questionData.correctAnswer
+			? userAnswer.classList.add('correct')
+			: userAnswer.classList.add('incorrect');
+
+		roundAndUserAnswers.textContent = `${questionData.correctAnswer} - `;
+		roundAndUserAnswers.append(userAnswer);
+
+		fragment.append(roundQuestion, roundAndUserAnswers);
+	});
+
+	questionContainer.append(fragment);
+};
+
+// cambiar de pregunta
+const nextQuestion = () => {
+	const questionData = QUESTIONS[round];
+	const answerElement = [...answersElements.children];
+
+	questionElement.textContent = questionData.question;
+
+	answerElement.forEach((answer, index) => {
+		answer.textContent = questionData.options[index];
+	});
+};
+
+// guardar la respuesta
+const answerClicked = event => {
+	if (!event.target.classList.contains('answer')) return;
+
+	answers.push(event.target.textContent);
+	console.log(answers);
+	round++;
+
+	if (round < QUESTIONS.length) {
+		nextQuestion();
+	} else {
+		checkAnswers();
+	}
+};
+
+// EVENTOS
+// =======
+answersElements.addEventListener('click', answerClicked);
